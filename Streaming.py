94a5bf04cf28ff833738e77dddd1dcbb0606647f
JN633 <jonemauriciocosta76@gmail.com>
2026-08-27 21:27:01 -0300

    informações de cadastro

--- Streaming.py
+++ Streaming.py
@@ -1,3 +1,17 @@
+
 from Assinante import Assinante
 class PlataformaStreaming:
-    
+    assinantes = []
+
+    def cadastrar(self):
+        plano = {
+             "1"|"Basico",
+             "2"|"Padrão"
+             "3"|"Premium"
+        }
+
+        nome = input("Informe o nome do assinante: ")
+        print("Selecione o plano: [1] Basico | [2] Padrão | [3] Premium")
+        plano_selecionado = input(">")
+        senha = input("Informe sua senha: ")
+        
